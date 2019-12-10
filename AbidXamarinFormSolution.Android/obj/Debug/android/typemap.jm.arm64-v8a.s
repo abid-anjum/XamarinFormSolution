3fd4ce9565d4c791b47599a20d6c837e19a7e42a
abidhussain <abid786@gmail.com>
2019-12-10 15:26:55 +0300

    first commit

---
+++ AbidXamarinFormSolution.Android/obj/Debug/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 7e0df219a67c57b638405e5b7cf891ea24ed9666 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1284
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
